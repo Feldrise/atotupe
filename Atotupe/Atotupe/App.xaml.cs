2dea735ac3c2af0f0943027ac9eee5d61d2e8a84
Feldrise <admin@feldrise.com>
2018-09-01 17:46:38 +0200

    [Add] WalletSummaryPage

--- Atotupe/Atotupe/App.xaml.cs
+++ Atotupe/Atotupe/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Atotupe.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace Atotupe
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new WalletSummaryPage());
         }
 
         protected override void OnStart()
