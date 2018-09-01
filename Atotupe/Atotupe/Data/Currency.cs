db67824e4d2400dd01fac434386309e3aba4bd8d
Feldrise <admin@feldrise.com>
2018-09-01 13:57:15 +0200

    [Add] Currency

---
+++ Atotupe/Atotupe/Data/Currency.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Atotupe.Data
+{
+    public class Currency : INotifyPropertyChanged
+    {
+        private string _code;
+        private string _name;
+        private double _number;
+        private double _value;
+
+        public string Code
+        {
+            get => _code;
+            set
+            {
+                _code = value;
+                OnPropertyChanged("Code");
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public double Number
+        {
+            get => _number;
+            set
+            {
+                _number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+
+        public double Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
