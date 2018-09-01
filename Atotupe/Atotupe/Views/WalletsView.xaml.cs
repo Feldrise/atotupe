693b19f396b8ac736db880af59c5a2885517c281
Feldrise <admin@feldrise.com>
2018-09-01 17:34:10 +0200

    [Add] WalletsView

---
+++ Atotupe/Atotupe/Views/WalletsView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atotupe.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Atotupe.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WalletsView : ContentView
+	{
+	    private ObservableCollection<Wallet> _wallets;
+
+	    public ObservableCollection<Wallet> Wallets
+	    {
+	        get => _wallets;
+	        set => _wallets = value;
+	    }
+
+		public WalletsView ()
+		{
+			InitializeComponent ();
+            GenerateWallets();
+
+		    ListOfWallets.ItemsSource = _wallets;
+		}
+
+	    private void GenerateWallets()
+	    {
+            //TODO: generate from save
+	        _wallets = new ObservableCollection<Wallet>
+	        {
+	            new Wallet {Name = "Wallet 1"},
+	            new Wallet {Name = "Wallet 2"},
+	            new Wallet {Name = "Wallet 3"}
+	        };
+	    }
+	}
+}
